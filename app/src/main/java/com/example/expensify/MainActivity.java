ab34ecb85950f6f3a8f294531876bb9f910c42c3
vanthuy1404 <tranphuongxinh27@gmail.com>
2024-03-23 20:51:42 +0700

    trang bao cao

--- app/src/main/java/com/example/expensify/MainActivity.java
+++ app/src/main/java/com/example/expensify/MainActivity.java
@@ -20,7 +20,6 @@ public class MainActivity extends AppCompatActivity {
     private BottomNavigationView bottomNavigationView;
 
     private Fragment addFragment, tradeFragment, reportFragment, userFragment;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
