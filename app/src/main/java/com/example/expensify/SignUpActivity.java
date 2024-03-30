047bf6e309c8c379d217379ad65fa6f65a4fee5e
ThanhGates <tranthanh27042002@gmail.com>
2024-03-30 13:04:36 +0700

    Commit login, signup, profile

---
+++ app/src/main/java/com/example/expensify/SignUpActivity.java
@@ -0,0 +1,87 @@
+package com.example.expensify;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    private EditText signupEmail, signupPassword, signupConfirmPassword;
+    private Button signupButton;
+    private TextView loginRedirectText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        auth = FirebaseAuth.getInstance();
+        signupEmail = findViewById(R.id.signup_email);
+        signupPassword = findViewById(R.id.signup_password);
+        signupConfirmPassword = findViewById(R.id.signup_confirm_password);
+        signupButton = findViewById(R.id.signup_button);
+        loginRedirectText = findViewById(R.id.loginRedirectText);
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = signupEmail.getText().toString().trim();
+                String pass = signupPassword.getText().toString().trim();
+                String confirmPass = signupConfirmPassword.getText().toString().trim();
+
+                if (user.isEmpty()){
+                    signupEmail.setError("Email cannot be empty");
+                    signupEmail.requestFocus();
+                }
+                if (pass.isEmpty()){
+                    signupPassword.setError("Password cannot be empty");
+                    signupPassword.requestFocus();
+                }
+                if (confirmPass.isEmpty()){
+                    signupConfirmPassword.setError("Confirm password cannot be empty");
+                    signupConfirmPassword.requestFocus();
+                }
+                if (!pass.equals(confirmPass)){
+                    signupPassword.setError("Password is not same confirm password");
+                    signupConfirmPassword.setError("Confirm password is not same password");
+                    signupPassword.requestFocus();
+                }
+                else{
+                    auth.createUserWithEmailAndPassword(user, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(SignUpActivity.this, "SignUp Successful", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(SignUpActivity.this, LoginActivity.class));
+                            } else {
+                                Toast.makeText(SignUpActivity.this, "SignUp Failed" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+
+            }
+        });
+
+        loginRedirectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SignUpActivity.this, LoginActivity.class));
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/example/expensify/UserFragment.java
+++ app/src/main/java/com/example/expensify/UserFragment.java
@@ -1,17 +1,23 @@
 package com.example.expensify;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -28,9 +34,15 @@ public class UserFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
+
+
+
+
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    FirebaseFirestore database;
 
     public UserFragment() {
         // Required empty public constructor
@@ -81,12 +93,107 @@ public class UserFragment extends Fragment {
             }
         });
 
-    }
 
+    }
+    View view;
+    Button btnEdit, btnExit;
+    TextView titleName,titleUsername,profileName, profileEmail, profileUsername,profileTelephone;
+
+    private LinearLayout editLayout;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_user_fragment, container, false);
+        btnEdit = view.findViewById(R.id.editButton);
+        btnExit = view.findViewById(R.id.exitButton);
+
+        titleName = view.findViewById(R.id.titleName);
+        titleUsername = view.findViewById(R.id.titleUsername);
+        profileName = view.findViewById(R.id.profileName);
+        profileEmail = view.findViewById(R.id.profileEmail);
+        profileUsername = view.findViewById(R.id.profileUsername);
+        profileTelephone = view.findViewById(R.id.profileTelephone);
+
+        DocumentReference docRef = FirebaseFirestore.getInstance().collection("user").document("oV7QOeQQAAY703JebyXk");
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot doc = task.getResult();
+                    if (doc.exists()) {
+                        String username = doc.getString("username");
+                        Log.d("Document", "Username: " + username);
+                        // Update the UI element with the username
+                        titleUsername.setText(username);
+                        profileUsername.setText(username);
+
+                        String email = doc.getString("email");
+                        Log.d("Document", "Email: " + email);
+                        profileEmail.setText(email);
+
+                        String name = doc.getString("name");
+                        Log.d("Document", "Name: " + name);
+                        titleName.setText(name);
+                        profileName.setText(name);
+
+                        String telephone = doc.getString("telephone");
+                        Log.d("Document", "Telephone: " + telephone);
+                        profileTelephone.setText(telephone);
+                    } else {
+                        Log.d("Document", "No data");
+                    }
+                } else {
+                    // Handle error if the task is not successful
+                    Log.d("Document", "Error: " + task.getException());
+                }
+            }
+        });
+        btnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                exitAccount();
+            }
+        });
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editProfile();
+            }
+        });
+        return view;
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_fragment, container, false);
+        //return inflater.inflate(R.layout.fragment_user_fragment, container, false);
+    }
+
+    private void exitAccount() {
+        // Clear the user's authentication session
+        FirebaseAuth.getInstance().signOut();
+
+        // Finish the current activity and return to the login screen
+        getActivity().finish();
+        startActivity(new Intent(getActivity(), LoginActivity.class));
+    }
+//    public void onLogOutButtonClick(View view) {
+//        // Perform the necessary action to log out the user and go back to the login screen
+//
+//        // Get the FragmentManager and perform a popBackStack() to go back to the login screen
+//        FragmentManager manager = getFragmentManager();
+//        if (manager != null) {
+//            manager.popBackStack();
+//        }
+//
+//        // Sign out the user
+//        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+//        mAuth.signOut();
+//
+//        // Redirect to the login screen
+//        Intent intent = new Intent(getActivity(), LoginActivity.class);
+//        startActivity(intent);
+//        getActivity().finish();
+//    }
+
+    public void editProfile(){
+        Intent intent = new Intent(getActivity(), EditProfileActivity.class);
+        startActivity(intent);
     }
 }
