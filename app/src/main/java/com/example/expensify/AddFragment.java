b7a02cc75c26e90e4a74b133198903bb25f312f3
Krackhet <krackhet188@gmail.com>
2024-04-15 00:20:03 +0700

    put userid into database

--- app/src/main/java/com/example/expensify/AddFragment.java
+++ app/src/main/java/com/example/expensify/AddFragment.java
@@ -188,6 +188,7 @@ public class AddFragment extends Fragment {
         expense.put("note", expenseContent);
         expense.put("category_detail", expenseSelectionCategory);
         expense.put("category_id", categoryID);
+        expense.put("user_id", SignUpActivity.user_id);
 
         AlertDialog ad = new AlertDialog.Builder(getActivity())
                 .create();
